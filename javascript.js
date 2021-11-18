9326a324473e6547f00bcb422d73d06bf57d561d
priKSouza <priscilakarinajump@gmail.com>
2021-11-18 12:31:21 -0300

    editando projeto

--- javascript.js
+++ javascript.js
@@ -1,21 +1,11 @@
-const titulo = document.getElementById("nome - projeto");
+const titulo = document.getElementById("nome-projeto");
 const sinopse = document.getElementById("resumo");
 const valorArtes = document.getElementById("valor-aprovado");
 const captadoArtes = document.getElementById("valor-captado");
 
-const segundoProjeto = document.getElementById("projeto-2");
-const sinopseSegundoProjeto = document.getElementById("resumo-2");
-const valorSegundoProjeto = document.getElementById("valor-aprovado-2");
-const captadoSegundoProjeto = document.getElementById("valor-captado-2");
-
-const terceiroProjeto = document.getElementById("projeto-3");
-const sinopseTerceiroProjeto = document.getElementById("resumo-3");
-const valorTerceiroProjeto = document.getElementById("valor-aprovado-3");
-const captadoTerceiroProjeto = document.getElementById("valor-captado-3");
-
 async function buscarAPI() {
   const response = await fetch(
-    "http://api.salic.cultura.gov.br/v1/projetos/?limit=6&format=json",
+    "http://api.salic.cultura.gov.br/v1/projetos/?limit=10&format=json",
     {
       method: "GET",
       headers: {
@@ -29,20 +19,10 @@ async function buscarAPI() {
   const json = await response.json();
   console.log(json);
 
-  titulo.innerText = json._embedded.projetos[2].area;
-  sinopse.innerText = json._embedded.projetos[18].resumo;
-  valorArtes.innerText = json._embedded.projetos[16].valor_aprovado;
-  captadoArtes.innerText = json._embedded.projetos[30].valor_captado;
-
-  segundoProjeto.innerText = json._embedded.projetos[34].area;
-  sinopseSegundoProjeto.innerText = json._embedded.projetos[50].resumo;
-  valorSegundoProjeto.innerText = json._embedded.projetos[48].valor_aprovado;
-  captadoSegundoProjeto.innerText = json._embedded.projetos[62].valor_captado;
-
-  terceiroProjeto.innerText = json._embedded.projetos[66].area;
-  sinopseTerceiroProjeto.innerText = json._embedded.projetos[82].resumo;
-  valorTerceiroProjeto.innerText = json._embedded.projetos[80].valor_aprovado;
-  captadoTerceiroProjeto.innerText = json._embedded.projetos[94].valor_captado;
+  titulo.textContent = json._embedded.projetos[2].area;
+  // sinopse.innerText = json._embedded.projetos[0].resumo;
+  // valorArtes.innerText = json._embedded.projetos.valor_aprovado;
+  // captadoArtes.innerText = json._embedded.projetos[0].valor_captado;
 }
 
 buscarAPI();
